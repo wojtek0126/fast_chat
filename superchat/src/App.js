5b7345b25c1bc275758b7d666cd5912128e60413
wojtek0126 <41778298+wojtek0126@users.noreply.github.com>
2021-08-29 18:34:09 +0200

    content updated

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -122,8 +122,9 @@ function SignIn() {
             }}> 
               <ScrollText>
                 This is my personal chat made with React.js and several of it's libraries. Feel free to sign in and type in whatever You want.
-                This app is evolving so next time around it will most likely look and work different. Same about the code and file structure
-                ...have fun!                 
+                This app is evolving so next time around it will most likely look and work different. The code and file structure
+                will also be getting improved. Right now this chat is free for anyone to test. Maximum number of comments is currently
+                set to 50...have fun!                 
               </ScrollText>         
           </Heading>  
     </>
